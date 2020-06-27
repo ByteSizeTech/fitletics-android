5c849ead5f659d57b5b056d6bd1dffd6c405f693
Vishal Haridas <supergamerhd101@gmail.com>
2020-06-27 04:21:41 +0400

    Intent to change activities on-tap of progressbar

--- app/src/main/java/com/example/fitletics/BTestOngoingActivity.kt
+++ app/src/main/java/com/example/fitletics/BTestOngoingActivity.kt
@@ -1,9 +1,11 @@
 package com.example.fitletics
 
 import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_baseline_test_ongoing.*
+import kotlinx.android.synthetic.main.activity_baseline_test_qr_scanner.*
 
 class BTestOngoingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +14,22 @@ class BTestOngoingActivity : AppCompatActivity() {
 
 
         //boiler plate code for progress bar below
-        progressBar.max = 10
+        btest_ongoing_pb.max = 10
         val currentProgress = 10
 
-        ObjectAnimator.ofInt(progressBar, "progress", currentProgress).setDuration(3500).start()
+        ObjectAnimator.ofInt(btest_ongoing_pb, "progress", currentProgress).setDuration(3500).start()
+
+        /*===========TEMPORARY CODE FOR TESTING===========*/
+
+        //changes the activity from by tapping the progress bar (for now)
+
+        btest_ongoing_pb.setOnClickListener()
+        {
+            val intent = Intent(this, BTestCompleteActivity::class.java)
+            startActivity(intent)
+        }
+
+        /*===============================================*/
 
     }
 }
