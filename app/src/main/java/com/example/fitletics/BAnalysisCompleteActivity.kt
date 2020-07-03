0b5fa87b3229c0d865dc0ea90cf9d2e0df1d92ea
Vishal Haridas <supergamerhd101@gmail.com>
2020-07-03 14:14:57 +0400

    Added BAnalysis completed activity

---
+++ app/src/main/java/com/example/fitletics/BAnalysisCompleteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fitletics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BAnalysisCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_body_analysis_complete)
+    }
+}
